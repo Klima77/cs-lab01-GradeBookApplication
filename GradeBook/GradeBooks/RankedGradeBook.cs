978ca41200ce47f56455c8cf08259fcb9677bfed
Klima77 <kacper.klimczak@microsoft.wsei.edu.pl>
2023-03-15 22:48:08 +0100

    Override `RankedGradeBook`'s `CalculateStatistics` method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,45 +8,57 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base (name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
         }
 
-    public override char GetLetterGrade(double averageGrade)
-    {
-        int numberOfStudents = Students.Count;
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int numberOfStudents = Students.Count;
 
-        if (numberOfStudents < 5)
-        {
-            throw new InvalidOperationException();
+            if (numberOfStudents < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int topTwentyPercent = (int)Math.Ceiling(numberOfStudents * 0.2);
+
+            List<double> grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+
+            if (averageGrade >= grades[topTwentyPercent - 1])
+            {
+                return 'A';
+            }
+            else if (averageGrade >= grades[(topTwentyPercent * 2) - 1])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= grades[(topTwentyPercent * 3) - 1])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= grades[(topTwentyPercent * 4) - 1])
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
 
-        int topTwentyPercent = (int)Math.Ceiling(numberOfStudents * 0.2);
-
-        List<double> grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
-
-        if (averageGrade >= grades[topTwentyPercent - 1])
+        public override void CalculateStatistics()
         {
-            return 'A';
+            int numberOfStudents = Students.Count;
+            if (numberOfStudents < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else if (numberOfStudents >= 5) 
+            {
+                base.CalculateStatistics();
+            }
         }
-        else if (averageGrade >= grades[(topTwentyPercent * 2) - 1])
-        {
-            return 'B';
-        }
-        else if (averageGrade >= grades[(topTwentyPercent * 3) - 1])
-        {
-            return 'C';
-        }
-        else if (averageGrade >= grades[(topTwentyPercent * 4) - 1])
-        {
-            return 'D';
-        }
-        else
-        {
-            return 'F';
-        }
-    }
-
     }
 }
